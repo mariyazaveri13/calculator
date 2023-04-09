94c7bb2726efab5d2710702c5f7a3b9772f34f24
Mariya Zaveri <mariyazaver13@gmail.com>
2023-04-09 17:31:56 +0530

    Added btns for clear and delete

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const currentLbl = document.getElementById('current');
 const historyLbl = document.getElementById('history');
 const operatorLbl = document.getElementById('operator');
 const opBtn = document.querySelectorAll('.op');
+const clearBtn = document.getElementById('clear');
+const deleteBtn = document.getElementById('delete');
 
 function operate(num1,num2,operator){
     
@@ -31,22 +33,22 @@ function operate(num1,num2,operator){
     return result;
 }
 
-function add(num1,num2){
+function add(num1=0,num2=0){
     console.log(num1 + num2);
     return num1 + num2;
 }
 
-function subs(num1, num2){
+function subs(num1=0, num2=0){
     console.log(num1 - num2);
     return num1 - num2;
 }
 
-function multiply(num1,num2){
+function multiply(num1=0,num2=1){
     console.log(num1 * num2);
     return num1 * num2;
 }
 
-function divide(num1,num2){
+function divide(num1=1,num2=0){
     console.log(num1 / num2);
     return num1 / num2;
 }
@@ -62,22 +64,24 @@ let oper = '';
 
 num.forEach( n =>{
     n.onclick = (e) =>{
-
-        if(opCount == 0){
-            num1 += e.target.value;
-            currentLbl.innerText = num1;
-        }
-
-        else if(opCount >= 1){
-            num2 += e.target.value;
-            currentLbl.innerText = num2;
-        }
-
+        updateLabels(e);
     }
 });
 
+function updateLabels(e){
+    if(opCount == 0){
+        num1 += e.target.value;
+        currentLbl.innerText = num1;
+    }
+    else if(opCount >= 1){
+        num2 += e.target.value;
+        currentLbl.innerText = num2;
+    }
+}
+
 opBtn.forEach( op =>{
     op.onclick = (e) =>{
+        
         opCount++;
 
         if(opCount == 1){
@@ -86,8 +90,11 @@ opBtn.forEach( op =>{
         }
 
         else if(opCount>1){
+            if(!num1 && !num2)
+                return;
+                
             if(e.target.value == '=')
-            {
+            {                
                 historyLbl.innerText = num1 + oper + num2 + e.target.value;
                 currentLbl.innerText = operate(num1,num2,oper);
                 num1 = '';
@@ -106,3 +113,38 @@ opBtn.forEach( op =>{
         }
     }
 });
+
+clearBtn.onclick = (e) => {
+    reset();
+}
+
+function reset(){
+    num1 = '';
+    num2 = '';
+    opCount = 0;
+    oper = '';
+    historyLbl.innerText = '';
+    currentLbl.innerText = '0';
+}
+
+deleteBtn.onclick = (e) => {
+    let curr = currentLbl.innerText;
+    
+    if(curr && Number(curr) > 0){
+        curr = curr.slice(0,-1);
+        currentLbl.innerText = curr;
+    }
+    else{
+        currentLbl.innerText = 0;
+    }
+}
+
+/*
+TODO
+
+1. extra decimal points not allowed
+2. if there is no value in num1 or num2 it shouldn't go to operate function.
+3. if after equal someone types in other num or something it should operate
+4. clear and delete btn
+
+*/
