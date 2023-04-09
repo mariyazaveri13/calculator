acd61daf742ea878e1f72a21ac8b612027190604
Mariya Zaveri <mariyazaver13@gmail.com>
2023-04-09 13:36:16 +0530

    the number onclicks are working as per requirement

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btns = document.querySelectorAll('.btn');
 const current = document.getElementById('current');
+const history = document.getElementById('history');
+const operator = document.getElementById('operator');
 
 function operate(num1,num2,operator){
     switch (operator){
@@ -37,10 +39,33 @@ function divide(num1,num2){
     result = num1 / num2;
 }
 
-let curr = '';
+let num1 = '';
+let op = '';
+let isNum1Filled = false;
+let num2 = '';
+let isOpFilled = false;
 btns.forEach(btn => {
     btn.onclick = (e) => {
-        curr += e.target.value;
-        current.innerText = curr;
+        if(btn.classList.contains('num') && !isNum1Filled)
+            num1 += e.target.value;
+        else if(btn.classList.contains('num') && isNum1Filled)
+            num2 += e.target.value;
+        else if(btn.classList.contains('op'))
+            op += e.target.value
+        
+        if(!isNum1Filled)
+            current.innerText = num1;
+
+        if(op && !isOpFilled){
+            isOpFilled = true;
+            isNum1Filled = true;
+            current.innerText = num2;
+            operator.innerText =  op;
+            history.innerText = num1;
+        }
+
+        if(isOpFilled){
+            current.innerText = num2;
+        }
     }
 });
